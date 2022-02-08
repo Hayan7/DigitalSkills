f6f3c7a54156e80678c2eb3216a9a0b1aeced32c
Hayan7 <hayan7abdel@gmail.com>
2022-02-08 12:42:08 +0100

    javascript

---
+++ Javascript/tutorial1.js
@@ -0,0 +1,10 @@
+let bruttoLoon = 2300;
+let bijdragSocialeZekerheid = bruttoLoon * 0.1307
+let bedrijfsVoorheffing = bruttoLoon * 0.1111
+let nettLoon = bruttoLoon -(bijdragSocialeZekerheid + bedrijfsVoorheffing)
+console.log(nettLoon)
+
+let weight = 70
+let height = 1.75
+let mBI = weight / Math.pow(1.75,2)
+console.log(mBI)
