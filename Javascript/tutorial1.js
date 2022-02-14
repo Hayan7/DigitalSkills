5df2d959e58a0e1d096f1f386a827954c8b2a6ff
Hayan7 <hayan7abdel@gmail.com>
2022-02-14 22:10:21 +0100

    some changes

--- Javascript/tutorial1.js
+++ Javascript/tutorial1.js
@@ -1,10 +1,38 @@
-let bruttoLoon = 2300;
-let bijdragSocialeZekerheid = bruttoLoon * 0.1307
-let bedrijfsVoorheffing = bruttoLoon * 0.1111
-let nettLoon = bruttoLoon -(bijdragSocialeZekerheid + bedrijfsVoorheffing)
-console.log(nettLoon)
+/*  */
 
-let weight = 70
-let height = 1.75
-let mBI = weight / Math.pow(1.75,2)
-console.log(mBI)
+/*function printerService(callBack){
+    return callBack
+}
+function getUser(username){
+    return username
+}
+let result = printerService(getUser('admin'))
+console.log(result)
+function student(object){
+
+}*/
+/* function askPermission(name,age){
+    return age < 18 ? `${name}! you are not allowed to drink alcohol`:`${name}!  Enjoy your drink!`
+}
+console.log(askPermission('Mark',20))
+console.log(askPermission('John',16)) */
+
+var person = {
+    "firstName": "Abdeljalil",
+    "secondName": "El Hayani Marzoqi",
+    "dateOfBirth": 1977,
+    "email" : "hayan@abdel#gmail.com",
+    "studies": ["history","Maths","Japanese"],
+    "hobbies" : {
+        "sport" : ["jogging","football"],
+        "leisure": "reading"
+    },
+    fullName:function(){
+        return this.firstName +" "+ this.secondName;
+    }
+}
+console.log(person.hobbies.sport[1])
+console.log(person.studies[1])
+console.log("I am " + person.fullName() +","+ "I was born in " + 
+person.dateOfBirth+ "." + " My email address is: "+ person.email+ "My hobbies are "+ 
+person.hobbies.sport[0] +" and "+ person.hobbies.leisure[0])
