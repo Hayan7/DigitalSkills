dce9e2d03612ce8eb94185770b3fcc9fa4ede4d9
Hayan7 <hayan7abdel@gmail.com>
2022-02-28 10:10:53 +0100

    pianoApp_changes

--- pianoapp/app.js
+++ pianoapp/app.js
@@ -1,25 +1,9 @@
-const   keys = document.querySelectorAll('.key'),
-    note = document.querySelector('.nowplaying');
-
 function playNote(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
           key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
     if(!key) return;
-   
-    const keyNote = key.getAttribute('data-note');
-
-    key.classList.add('playing');
-    note.innerHTML = keyNote;
-    audio.currentTime = 0;
     audio.play();
 }
 
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return; //skip if not a transform animation
-    this.classList.remove('playing');
-}
-
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-window.addEventListener('keydown', playNote);
+document.addEventListener('click', playNote);
